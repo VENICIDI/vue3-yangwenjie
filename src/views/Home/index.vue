d5197f18466179fc175d13ca6ea8f309a70c7640
VENICIDI <you@example.com>
2024-12-12 18:34:21 +0800

    新鲜好物

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 
 
@@ -16,4 +17,12 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+
+  <HomePanel title="新鲜好物" sub-title="新鲜好无">
+    <div>bb</div>
+  </HomePanel>
+
+  <HomePanel title="人气推荐" sub-title="bn">
+    <div>cc</div>
+  </HomePanel>
 </template>
